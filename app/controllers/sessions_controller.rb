821c43653da275418923bbde7d564d12ba9900f6
Hank Fanchiu <hank@fanchiu.com>
2015-11-22 12:48:33 -0800

    Singularize user and session resources; refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,17 +11,17 @@ class SessionsController < ApplicationController
     @user = User.find_by_credentials(name, password)
     if @user
       login_user!(@user)
-      redirect_to users_url
+      redirect_to user_url
     else
       flash[:errors] = ["Login Failed..."]
-      redirect_to new_sessions_url
+      redirect_to new_session_url
     end
   end
 
   def destroy
     current_user.reset_session_token!
     session[:session_token] = nil
-    redirect_to new_sessions_url
+    redirect_to new_session_url
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login_user!(@user)
-      redirect_to users_url
+      redirect_to user_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
