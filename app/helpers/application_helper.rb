b6aebf99fd59aab6693b134a675263c8918ddc6a
Hank Fanchiu <hank@fanchiu.com>
2015-11-22 20:31:23 -0800

    Refactor voting status (up, down, sum) to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,8 +24,32 @@ module ApplicationHelper
     "&darr;".html_safe
   end
 
+  def upvote(object)
+    if object.class.name == "Post"
+      action = upvote_post_url(object)
+    elsif object.class.name == "Comment"
+      action = upvote_comment_url(object)
+    end
+
+    link_to up_arrow, action, method: :post
+  end
+
+  def downvote(object)
+    if object.class.name == "Post"
+      action = downvote_post_url(object)
+    elsif object.class.name == "Comment"
+      action = downvote_comment_url(object)
+    end
+
+    link_to down_arrow, action, method: :post
+  end
+
   def vote_count(object)
     type, id = object.class.name, object.id
     Vote.where(votable_type: type, votable_id: id).sum(:value)
+  end
+
+  def voting(object)
+    "#{upvote(object)} #{downvote(object)} (#{vote_count(object)})".html_safe
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,8 +2,4 @@ module PostsHelper
   def top_level_comments
     @post.comments.where(parent_comment_id: nil)
   end
-
-  def subs_list
-
-  end
 end
