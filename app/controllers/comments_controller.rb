019be7aa2cd5b21ea900220ccf8ffc33c53f82c1
Hank Fanchiu <hank@fanchiu.com>
2015-11-22 16:09:59 -0800

    Add Comments controller and #create and #new routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    render :new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_url(@comment.post_id)
+    else
+      flash.now[:errors] = @comment.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root to: 'users#new'
 
   resource :user, only: [:new, :create, :show]
+
   resource :session, only: [:new, :create, :destroy]
 
   resources :subs, except: :destroy
+
   resources :posts, except: :index
+
+  resources :comments, only: [:new, :create]
 end
