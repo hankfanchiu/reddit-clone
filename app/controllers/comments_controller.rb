16c52573a91e6a7aa6af00ea849c5da792beb79d
Hank Fanchiu <hank@fanchiu.com>
2015-11-22 16:29:48 -0800

    Add commenting feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = current_user.comments.new(comment_params)
     if @comment.save
       redirect_to post_url(@comment.post_id)
     else
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :post_id)
   end
 end
